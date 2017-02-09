a8969f132801d0fcd28bff00a2c6ed2450adc149
Rodrigo <rodrigo19962010@live.com>
2017-02-09 08:31:31 -0300

    Resolução dos Flash Alerts

--- app/models/example.rb
+++ app/models/example.rb
@@ -1,5 +1,7 @@
 class Example < ActiveRecord::Base
      mount_uploader :my_file, FileUploader
-     
      belongs_to :folder
+
+     validates :name, :presence => true
+     validates :my_file, :presence => true
 end
--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,4 +1,6 @@
 class Folder < ActiveRecord::Base
   belongs_to :user
   has_many :examples, :dependent => :destroy
+
+  validates :name, :presence => true
 end
